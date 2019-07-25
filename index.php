334fb39c16604b88d27e907bafa4435e6d4ec4d2
baranyaimate <mateb06@gmail.com>
2019-07-25 10:42:00 +0200

    bugfix

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en" role="main">
+<html lang="hu" role="main">
 <head>
     <title></title>
-    <<?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
 </head>
 <body>
     <div class="container form-container">
