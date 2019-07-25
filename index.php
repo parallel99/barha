da93abaa3b1e87a07a2203a2bbff72b223fd2819
baranyaimate <mateb06@gmail.com>
2019-07-25 21:05:00 +0200

    gradient background check

--- index.php
+++ index.php
@@ -5,9 +5,39 @@
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
 </head>
 <body>
-    <div class="container form-container">
+        <!-- Grid row -->
+    <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-4">
+          <div class="purple-gradient color-block mb-3 mx-auto rounded-circle z-depth-1"></div>
+          <h5 class="mb-0">purple-gradient</h5>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-4">
+          <div class="blue-gradient color-block mb-3 mx-auto rounded-circle z-depth-1"></div>
+          <h5 class="mb-0">blue-gradient</h5>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-4">
+          <div class="aqua-gradient color-block mb-3 mx-auto rounded-circle z-depth-1"></div>
+          <h5 class="mb-0">aqua-gradient</h5>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-4">
+          <div class="peach-gradient color-block mb-3 mx-auto rounded-circle z-depth-1"></div>
+          <h5 class="mb-0">peach-gradient</h5>
+        </div>
+        <!-- Grid column -->
 
     </div>
+    <!-- Grid row -->
 </body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
 </html>
