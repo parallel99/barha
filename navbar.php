a4f8a49cd65b78cb8d46b34c3f4b18eba3b40804
baranyaimate <mateb06@gmail.com>
2019-07-29 15:48:03 +0200

    add navbar

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@
             <li class="nav-item active">
                 <a class="nav-link" href="#">Kezdőlap</a>
             </li>
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="#">Receptek</a>
             </li>
             <li class="nav-item">
