b7b2281efc5816fc81af256cbd1682e37296894a
baranyaimate <mateb06@gmail.com>
2019-07-29 10:01:04 +0200

    autocomplete test

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 <html lang="hu" role="main">
-<head>
-    <title></title>
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
-</head>
-<body>
-    <h1 id="title">Mi van a hűtőben?</h1>
+    <head>
+        <title></title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
+    </head>
+    <body>
+        <h1 id="title">Mi van a hűtőben?</h1>
 
-    <form>
-      <div class="form-group">
-             <input type="text" class="form-control" id="ingredients" placeholder="" tabindex="1">
-      </div>
-      <button type="submit" class="btn btn-primary">Keresés</button>
-    </form>
-    <script>
-
-    </script>
-</body>
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" id="ingredients" placeholder="">
+            </div>
+            <button type="submit" class="btn btn-primary">Keresés</button>
+        </form>
+    </body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
 </html>
