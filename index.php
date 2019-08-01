7ae3fad23e7a7edbf5926f1aaf23c0b2054b9a94
baranyaimate <mateb06@gmail.com>
2019-08-01 14:10:54 +0200

    add ingredients limit

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
         </form>
         <script>
             $('.ingredients-group').on('input', function (event) {
-                if ($("div.ingredients-group div:last-child > input").val() != "") {
-                    var length = $(".ingredients-group > div").length
+                var length = $(".ingredients-group > div").length
+                if ($("div.ingredients-group div:last-child > input").val() != "" && length < 25) {
                     $(".ingredients-group").append("<div class='form-group'><input type='text' class='form-control' name='ingredients" + (length + 1) + "' id='ingredients" + (length + 1) + "' placeholder='Hozzávalók'></div>");
                     $("body").css("background", "linear-gradient(40deg, #2096ff, #05ffa3)");
                     $("#ingredients" + (length + 1)).autocomplete({
