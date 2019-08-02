2aac70d361add12a76ea241a51e6ecd3355ff17e
baranyaimate <mateb06@gmail.com>
2019-08-02 15:32:47 +0200

    add search.php

--- search.php
+++ search.php
@@ -6,10 +6,12 @@
     </head>
     <body>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php'; ?>
-        <?php
-            $ingredients = array_unique(array_filter($_GET));
-            print_r($ingredients);
-        ?>
+        <div class="container">
+            <?php
+                $ingredients = array_unique(array_filter($_GET));
+                print_r($ingredients);
+            ?>
+        </div>
     </body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
 </html>
