cc2908c1a73f1aa3b1ae8aca8db6c32c2be3f981
baranyaimate <mateb06@gmail.com>
2019-08-02 14:46:31 +0200

    add search.php

---
+++ search.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="hu" role="main">
+    <head>
+        <title></title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'; ?>
+    </head>
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php'; ?>
+        <?php
+            print_r($_GET);
+        ?>
+    </body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.php'; ?>
+</html>
