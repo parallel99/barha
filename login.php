85847e6a9cd212ed04ab63bcd0c6894b5a936de3
baranyaimate <mateb06@gmail.com>
2019-08-04 18:08:24 +0200

    edit login form

--- login.php
+++ login.php
@@ -6,17 +6,19 @@
     </head>
     <body>
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.php'; ?>
-        <form method="post" class="shadow" id="loginForm">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-            </div>
-            <button type="submit" class="btn btn-primary">Bejelentkezés</button>
-        </form>
+        <div class="form-container">
+            <form method="post" class="shadow" id="loginForm">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Bejelentkezés</button>
+            </form>
+        </div>
         <?php
             if(isset($_POST['submit'])) {
 
