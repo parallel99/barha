0c6a2e5719701c6f8c241617bfad8afab9dce999
baranyaimate <mateb06@gmail.com>
2019-08-04 11:21:42 +0200

    autocomplete from db

---
+++ getIngredients.php
@@ -0,0 +1,9 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/db.php';
+
+$stmt = $pdo->prepare("SELECT name FROM ingredients");
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo "<script>console.log(\"" . $row . "\")</script>";
+?>
