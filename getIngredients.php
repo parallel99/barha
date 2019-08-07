1fded9899b29695b4e4062295b8ffe97b479a0ca
baranyaimate <mateb06@gmail.com>
2019-08-07 09:51:37 +0200

    autocomplete order

--- getIngredients.php
+++ getIngredients.php
@@ -2,7 +2,7 @@
 
 include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 
-$stmt = $pdo->prepare("SELECT name FROM ingredients");
+$stmt = $pdo->prepare("SELECT name FROM ingredients ORDER BY name");
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo "<script>ingredients.push(\"" . $row['name'] . "\")</script>";
