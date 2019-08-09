54c6a8a82b2ecaea62275f0f3ce41a8b35c71cea
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-09 17:59:14 +0200

    Update 404.php

--- error/404.php
+++ error/404.php
@@ -1,1 +1,15 @@
-<h1>404</h1>
+<!DOCTYPE html>
+<html lang="hu" role="main">
+    <head>
+        <title></title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
+    </head>
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; ?>
+        <h1 id="title">Mi van a hűtőben?</h1>
+
+        <div class="shadow">
+        </div>
+    </body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
+</html>
