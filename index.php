b93733e7c1d7316d80b30149b6a18e91a295b042
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 14:50:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
     </head>
     <body>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; ?>
-        menu("index");
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; 
+            menu("index");
+        ?>
         <h1 id="title">Mi van a hűtőben?</h1>
 
         <form method="get" class="shadow" action="search.php">
