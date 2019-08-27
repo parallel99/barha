223025ef4c08ebbb4e4bd8ca78dc28c2bb3e5bbc
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 14:53:48 +0200

    Update search.php

--- search.php
+++ search.php
@@ -5,7 +5,10 @@
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
     </head>
     <body>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; ?>
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; 
+            menu("index");
+        ?>
         <div class="container">
             <?php
                 $ingredients = array_unique(array_filter($_GET));
