99a336ff3985f55fb097768a425fba88ae57b3e5
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 14:47:43 +0200

    Update navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -1,3 +1,6 @@
+<?php
+function menu($active) {
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="index.php">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -5,18 +8,21 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
-            <li class="nav-item active">
+             <li class="nav-item <?php if($active == "index"){ echo "active";}?>">
                 <a class="nav-link" href="index.php">Kezdőlap</a>
-            </li>
-            <li class="nav-item">
+             </li>
+             <li class="nav-item <?php if($active == "recipe"){ echo "active";}?>">
                 <a class="nav-link" href="#">Receptek</a>
-            </li>
-            <li class="nav-item">
+             </li>
+             <li class="nav-item <?php if($active == "registration"){ echo "active";}?>">
                 <a class="nav-link" href="registration">Regisztráció</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?php if($active == "login"){ echo "active";}?>">
                 <a class="nav-link" href="login.php">Bejelentkezés</a>
             </li>
         </ul>
     </div>
 </nav>
+<?php
+}
+?>
