3cadb8ce2b380d47b1469d9e11f0ad9cfcc8d750
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 20:24:09 +0200

    😢 😭😢 😭
    here is the black magic happened

--- getIngredients.php
+++ getIngredients.php
@@ -4,14 +4,11 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 
 $stmt = $pdo->prepare("SELECT name FROM ingredients ORDER BY name");
 $stmt->execute();
-$tomb = array();
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    array_push($tomb, $row['name']);
-    //echo "<script>ingredients.push(\"" . $row['name'] . "\")</script>";
-}
 echo "<script>ingredients.push(";
-foreach ($tomb as $hozzavalok) {
-  echo '"'.$hozzavalok.'",';
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo '"'.$row['name']'",';
 }
 echo ")</script>";
+
+
 ?>
