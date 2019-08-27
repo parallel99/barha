b95139bd1a138ffae33a31c20b5f21959bfa722f
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 20:26:07 +0200

    here is the black magic happened
    😢 😭

--- getIngredients.php
+++ getIngredients.php
@@ -6,7 +6,7 @@ $stmt = $pdo->prepare("SELECT name FROM ingredients ORDER BY name");
 $stmt->execute();
 echo "<script>ingredients.push(";
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo '"'.$row['name']'",';
+    echo '"'.$row['name'].'",';
 }
 echo ")</script>";
 
