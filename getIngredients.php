fb5eea2583cced877c3870c1f52e83040980ce4a
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 19:49:28 +0200

    Update getIngredients.php

--- getIngredients.php
+++ getIngredients.php
@@ -4,8 +4,8 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 
 $stmt = $pdo->prepare("SELECT name FROM ingredients ORDER BY name");
 $stmt->execute();
+$tomb = array();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $tomb = array();
     array_push($tomb, $row['name']);
     echo "<script>ingredients.push(\"" . $row['name'] . "\")</script>";
 }
