42059be16582fd7a9938d8c518ae8e7c0a312009
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-27 14:50:41 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,7 +5,10 @@
         <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
     </head>
     <body>
-        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; ?>
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php'; 
+            menu("registration");
+        ?>
         <div class="form-container">
             <form method="post" class="shadow" id="registrationForm">
                 <div class="form-group">
