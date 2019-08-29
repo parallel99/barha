c25c386e669bdfb6894b305329e6fe4b131642a2
parallel99 <46271427+parallel99@users.noreply.github.com>
2019-08-29 17:32:36 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -41,8 +41,8 @@
             <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                     <h4 class="modal-title">Felhasználási feltételek</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
                     <p>Some text in the modal.</p>
