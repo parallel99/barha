1e25f95851801884d2fe3f7caf325fe747a047ec
Erik <assassinseries@gmail.com>
2019-08-30 21:29:37 +0200

    whyyyyyy

--- registration.php
+++ registration.php
@@ -99,7 +99,7 @@
 
               include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 
-              $getemail = $pdo->prepare("SELECT `id` FROM `users` WHERE `email` = ':email'");
+              $getemail = $pdo->prepare("SELECT id FROM users WHERE email = :email;");
               $getemail->bindParam(':email', $email, PDO::PARAM_STR);
               $getemail->execute();
               $row = $getemail->get_result();
