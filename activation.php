a895f6f35ac4a835f5777d7562a89d06c2150123
Erik <assassinseries@gmail.com>
2019-08-31 21:25:10 +0200

    Test

--- activation.php
+++ activation.php
@@ -10,16 +10,17 @@ include ($_SERVER['DOCUMENT_ROOT']. '/include/db.php');
 $stmt = $pdo->prepare("SELECT active FROM users WHERE token = :token");
 $stmt->bindValue(':token', $_GET["id"], PDO::PARAM_STR);
 $stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_OBJ);
 $row = $stmt->fetchColumn();
+$user = $stmt->fetch(PDO::FETCH_OBJ);
 print_r($user->active);
+
 if($row == 1){
   if ($user->active == 1) {
       $volt = true;
   } else {
       $volt = false;
-      $activate = $db->prepare("UPDATE users SET active = '1' WHERE token = :token");
-      $stmt->bindValue(':token', $_GET["id"], PDO::PARAM_STR);
+      $activate = $pdo->prepare("UPDATE users SET active = '1' WHERE token = :token");
+      $activate->bindValue(':token', $_GET["id"], PDO::PARAM_STR);
       $activate->execute();
   }
 }
