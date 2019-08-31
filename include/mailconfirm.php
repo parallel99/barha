648c163ef7f78b9c2445832f75d79d78c5edff6c
Erik <assassinseries@gmail.com>
2019-08-31 20:57:44 +0200

    Mail test

--- include/mailconfirm.php
+++ include/mailconfirm.php
@@ -5,11 +5,11 @@
           <head>
           </head>
           <dody>
-          <h2>Kedves <?php echo $username;?>!</h2>
+          <h2>Kedves '.$username.'>!</h2>
           <strong>
             Mivel regisztráltál nálunk, ezért automatán generáltunk neked egy megerősítő email-t!
           </strong>
-            Az alábbi linken tudod megerősíteni a regisztrációdat: <a href="https://etel-seged.herokuapp.com/activation?id=<?php echo $tokenid;?>">Kattintson ide!</a>
+            Az alábbi linken tudod megerősíteni a regisztrációdat: <a href="https://etel-seged.herokuapp.com/activation?id='.$tokenid.'">Kattintson ide!</a>
             Ha nem te regisztráltál akkor hagyd figyelmen kívül ezt az email-t!(Máté ezt még fejleszteni kell)
           </body>
         </html>';
