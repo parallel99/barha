2f7c06467a96ec1e81ae5b2289324db9145f4b32
baranyaimate <mateb06@gmail.com>
2019-08-31 13:49:33 +0200

    sendgrid test

--- phpinfo.php
+++ phpinfo.php
@@ -18,7 +18,7 @@ if(isset($_POST['email'])) {
     $email = new \SendGrid\Mail\Mail();
     $email->setFrom("test@example.com", "Example User");
     $email->setSubject("Sending with SendGrid is Fun");
-    $email->addTo("test@example.com", "Example User");
+    $email->addTo("mateb06@gmail.com", "Example User");
     $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
     $email->addContent(
         "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
