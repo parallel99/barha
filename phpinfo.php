98798556f29912cf5e948a92d1b4f8aa3a37b0d9
baranyaimate <mateb06@gmail.com>
2019-08-31 13:53:21 +0200

    sendgrid test

--- phpinfo.php
+++ phpinfo.php
@@ -1,22 +1,9 @@
 <? phpinfo(); ?>
 
-<form method="post">
-    <input type="submit" name="email" id="email">
-</form>
-
 <?
-
-if(isset($_POST['email'])) {
-    require 'vendor/autoload.php'; // If you're using Composer (recommended)
-    // Comment out the above line if not using Composer
-    // require("<PATH TO>/sendgrid-php.php");
-    // If not using Composer, uncomment the above line and
-    // download sendgrid-php.zip from the latest release here,
-    // replacing <PATH TO> with the path to the sendgrid-php.php file,
-    // which is included in the download:
-    // https://github.com/sendgrid/sendgrid-php/releases
+    require 'vendor/autoload.php';
     $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("test@example.com", "Example User");
+    $email->setFrom("test@example.com", "étel-segéd");
     $email->setSubject("Sending with SendGrid is Fun");
     $email->addTo("mateb06@gmail.com", "Example User");
     $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
@@ -32,6 +19,4 @@ if(isset($_POST['email'])) {
     } catch (Exception $e) {
         echo 'Caught exception: '. $e->getMessage() ."\n";
     }
-}
-
 ?>
