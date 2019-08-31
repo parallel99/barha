72681e255308599c2af11edd434a7d4c6c99b18a
Erik <assassinseries@gmail.com>
2019-08-31 16:22:51 +0200

    add mail send in registration test

---
+++ include/mail-send.php
@@ -0,0 +1,33 @@
+<?php
+class Mail {
+
+  public $name;
+  public $email;
+  public $message;
+
+  public function __construct($name, $email, $message) {
+      $this->name = $name;
+      $this->$emailaddress = $email;
+      $this->message = $message;
+  }
+
+  public function Send() {
+      require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+      $email = new \SendGrid\Mail\Mail();
+      $email->setFrom("etel@seged.com", "étel-segéd");
+      $email->setSubject("Ez a cím!");
+      $email->addTo($this->$emailaddress, $name);
+      //$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+      $email->addContent("text/html", $this->message);
+
+      $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+      try {
+          $response = $sendgrid->send($email);
+          print $response->statusCode() . "\n";
+          print_r($response->headers());
+          print $response->body() . "\n";
+      } catch (Exception $e) {
+          echo 'Caught exception: '. $e->getMessage() ."\n";
+      }
+  }
+}
