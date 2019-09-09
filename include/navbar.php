6f0490117c7af4c875544554efe0972a8346edce
Erik <assassinseries@gmail.com>
2019-09-09 20:49:35 +0200

    menu test

--- include/navbar.php
+++ include/navbar.php
@@ -41,15 +41,15 @@ function menu($active) {
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
-             <li class="nav-item <?php //if($active == "index"){ echo "active";}?>">
+             <li class="nav-item <?php if($active == "index"){ echo "active";}?>">
                 <a class="nav-link" href="index.php">Kezdőlap</a>
              </li>
-             <li class="nav-item <?php //if($active == "recipe"){ echo "active";}?>">
+             <li class="nav-item <?php if($active == "recipe"){ echo "active";}?>">
                 <a class="nav-link" href="#">Receptek</a>
              </li>
              <li class="nav-item dropdown">
                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 <?php //echo $_SESSION['user']['name'];?>
+                 <?php echo $_SESSION['user']['name'];?>
                </a>
                <form class="dropdown-menu" method="post" aria-labelledby="navbarDropdown">
                  <a class="dropdown-item" href="#">Profil</a>
