dbae6a9ef2a5274b520850e164506fe127a7edd1
baranyaimate <mateb06@gmail.com>
2019-09-10 21:19:15 +0200

    email confirm redesign

--- activation.php
+++ activation.php
@@ -4,7 +4,7 @@ if (!isset($_GET["id"])) {
     die();
 }
 
-include ($_SERVER['DOCUMENT_ROOT']. '/include/db.php');
+include ($_SERVER['DOCUMENT_ROOT'] . '/include/db.php');
 
 
 $stmt = $pdo->prepare("SELECT * FROM users WHERE token = :token");
@@ -13,15 +13,15 @@ $stmt->execute();
 $row = $stmt->rowCount();
 $user = $stmt->fetch(PDO::FETCH_OBJ);
 
-if($row == 1){
-  if ($user->active == 1) {
-      $volt = true;
-  } else {
-      $volt = false;
-      $activate = $pdo->prepare("UPDATE users SET active = '1' WHERE token = :token");
-      $activate->bindValue(':token', $_GET["id"], PDO::PARAM_STR);
-      $activate->execute();
-  }
+if ($row == 1) {
+    if ($user->active == 1) {
+        $volt = true;
+    } else {
+        $volt = false;
+        $activate = $pdo->prepare("UPDATE users SET active = '1' WHERE token = :token");
+        $activate->bindValue(':token', $_GET["id"], PDO::PARAM_STR);
+        $activate->execute();
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -32,28 +32,28 @@ if($row == 1){
     </head>
     <body>
         <?php
-            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
-            menu("none");
+        include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
+        menu("none");
         ?>
         <div class="form-container">
             <div class="shadow email-confirm">
                 <?php
-                  if (isset($volt)) {
-                      if ($volt) {
+                if (isset($volt)) {
+                    if ($volt) {
                         echo "<h1>Sikertelen megerősítés</h1><hr>";
                         echo "<p>A megerősítés már megtörtént!</p>";
-                      } else {
+                    } else {
                         echo "<h1>Sikeres megerősítés</h1><hr>";
                         echo "<p>Sikeresen megerősítette a regisztrációt!</p>";
-                      }
-                  } else {
+                    }
+                } else {
                     echo "<h1>Sikeres megerősítés</h1><hr>";
                     echo "<p>Érvénytelen link!</p>";
-                  }
+                }
                 ?>
                 <hr>
                 <div>
-                  <a href="/" class="btn btn-primary">Főoldal</a>
+                    <a href="/" class="btn btn-primary">Főoldal</a>
                 </div>
             </div>
         </div>
