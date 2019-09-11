40604cad36c8c1e906e869a33ba4343406d9242d
Erik <assassinseries@gmail.com>
2019-09-11 16:42:31 +0200

    menu test

--- include/navbar.php
+++ include/navbar.php
@@ -34,32 +34,20 @@ function menu($active) {
 <?php
 } else {
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-             <li class="nav-item <?php if($active == "index"){ echo "active";}?>">
-                <a class="nav-link" href="index.php">Kezdőlap</a>
-             </li>
-             <li class="nav-item <?php if($active == "recipe"){ echo "active";}?>">
-                <a class="nav-link" href="#">Receptek</a>
-             </li>
-             <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 <?php echo $_SESSION['user']['name'];?>
-               </a>
-               <form class="dropdown-menu" method="post" aria-labelledby="navbarDropdown">
-                 <div class="fa fa-user-o"><a class="dropdown-item" href="#">Profil</a></div>
-                 <div class="fa fa-sliders"><a class="dropdown-item" href="#">Valami</a></div>
-               <div class="dropdown-divider"></div>
-                 <div class="fa fa-sign-out"><button type="submit" name="logout" class="dropdown-item">Kijelentkez</button></div>
-               </form>
-            </li>
-        </ul>
+<nav class="navbar navbar-toggleable-sm navbar-inverse bg-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Acme</a>
+  <div class="collapse navbar-collapse justify-content-between" id="navbar">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">Ball Bearings</a>
+      <a class="nav-item nav-link" href="#">TNT Boxes</a>
     </div>
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">Logout</a>
+    </div>
+  </div>
 </nav>
 <?php
   }
