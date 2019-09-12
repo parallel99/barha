7cb75db5a99fdca93e9b6f2b1d8f8d041b4b7079
Erik <assassinseries@gmail.com>
2019-09-12 16:43:47 +0200

    menu test

--- include/navbar.php
+++ include/navbar.php
@@ -34,33 +34,45 @@ function menu($active) {
 <?php
 } else {
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-             <li class="nav-item <?php if($active == "index"){ echo "active";}?>">
-                <a class="nav-link" href="index.php">Kezdőlap</a>
-             </li>
-             <li class="nav-item <?php if($active == "recipe"){ echo "active";}?>">
-                <a class="nav-link" href="#">Receptek</a>
-             </li>
-             <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 <?php echo $_SESSION['user']['name'];?>
-               </a>
-               <form class="dropdown-menu" method="post" aria-labelledby="navbarDropdown">
-                 <div class="fa fa-user-o"><a class="dropdown-item" href="#">Profil</a></div>
-                 <div class="fa fa-sliders"><a class="dropdown-item" href="#">Valami</a></div>
-               <div class="dropdown-divider"></div>
-                 <div class="fa fa-sign-out"><button type="submit" name="logout" class="dropdown-item">Kijelentkez</button></div>
-               </form>
-            </li>
-        </ul>
-    </div>
-</nav>
+<div id="app" class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarNavDropdown" class="navbar-collapse collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
 <?php
   }
 }
