35b5d9963ec022cfa0817efe9b360e4329f12aa7
baranyaimate <mateb06@gmail.com>
2019-09-15 20:47:32 +0200

    add profile.php

--- profile.php
+++ profile.php
@@ -57,7 +57,7 @@
                 <?php
                     if(isset($_POST['account-delete'])) {
                         include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-                        $email     = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+                        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
                         $password = hash('sha512', filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING));
 
                         $stmt = $pdo->prepare("SELECT * FROM users WHERE email= :email AND password = :password");
@@ -66,6 +66,9 @@
                         $stmt->execute();
                         $user = $stmt->fetch(PDO::FETCH_OBJ);
                         $row = $stmt->rowCount();
+
+                        echo "email: " . $email;
+                        echo "pass" . $password;
 
                         if ($row == 1) {
                             echo "jo jelszo";
