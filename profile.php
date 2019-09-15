6911cd464e7f2c2e13df4ed659bfcb5b1fb89e5c
baranyaimate <mateb06@gmail.com>
2019-09-15 20:54:58 +0200

    add profile.php

--- profile.php
+++ profile.php
@@ -71,13 +71,33 @@
                         echo "pass" . $password;
 
                         if ($row == 1) {
-                            echo "jo jelszo";
+                            echo "<script> $('#deleteConfirmModal').modal('show');</script>";
                         } else {
-                            echo "nem jo";
+                            echo "hibas jelszo"
                         }
                         echo "rc: " . $row;
                     }
                 ?>
+            </div>
+            <!-- deleteConfirmModal -->
+            <div class="modal fade" id="deleteConfirmModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Fiók törlése</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                      Biztos benne hogy törölni szeretne a fiókját? Ezek után a fiók soha többé nem lesz visszaállítható.
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Mégsem</button>
+                    <button type="button" class="btn btn-danger">Igen</button>
+                  </div>
+                </div>
+              </div>
             </div>
     </body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
