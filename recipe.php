e1da7176aae967e07a4b85d35b5d1adf57a5de53
baranyaimate <mateb06@gmail.com>
2019-10-08 12:40:37 +0200

    add recipe.php

---
+++ recipe.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="hu" role="main">
+    <head>
+        <title>BárHa | </title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
+    </head>
+    <body>
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
+            menu("none");
+        ?>
+        <div class="container">
+            
+        </div>
+    </body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
+</html>
