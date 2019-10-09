a832155c0ce78c1edb5b12087b02e800a592b0e6
Erik <assassinseries@gmail.com>
2019-10-09 19:36:04 +0200

    fix cookie alert

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
-<?php if (!isset($_COOKIE["cookie-OK"]) || $_COOKIE["cookie-OK"] != 1) {?>
+<?php if (!isset($_COOKIE["cookieok"]) || $_COOKIE["cookieok"] != 1) {?>
 <div class="alert alert-primary cookie-alert">
   Az oldalon <strong>sütiket használunk</strong> annak érdekében, hogy a lehető legjobb felhasználói élményt nyújtsuk.
   <form method="post" style="display: inline"><button type="submit" name="cookie-OK" class="btn btn-primary">Elfogad</button></form>
