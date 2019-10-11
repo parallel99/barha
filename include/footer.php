184b7fc1603a7a36256ac58cc512626ca2eb6570
Erik <assassinseries@gmail.com>
2019-10-11 22:04:34 +0200

    cookie js

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <?php if (!isset($_COOKIE["cookieok"]) || $_COOKIE["cookieok"] != 1) {?>
 <div class="alert alert-primary cookie-alert">
   Az oldalon <strong>sütiket használunk</strong> annak érdekében, hogy a lehető legjobb felhasználói élményt nyújtsuk.
-  <div style="display: inline" class="btn btn-primary cookie">Elfogad</div>
+  <div style="display: inline-block; cursor: pointer" class="btn btn-primary cookie">Elfogad</div>
 </div>
 <script>
 $(document).ready(function(){
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $(".cookie-alert").hide(300, "swing");
     var now = new Date();
     var time = now.getTime();
-    var expireTime = time + 3600*576*7;
+    var expireTime = time + 3600*576*7*30*12*5;
     now.setTime(expireTime);
     document.cookie = 'cookieok=1;expires='+now.toGMTString()+';path=/';
   });
