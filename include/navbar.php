0dcad84974175996d71b780d04449929f5cd0587
baranyaimate <mateb06@gmail.com>
2019-10-11 21:56:53 +0200

    add recipes page

--- include/navbar.php
+++ include/navbar.php
@@ -47,7 +47,7 @@ function menu($active) {
              <a class="nav-link" href="index">Kezdőlap</a>
           </li>
           <li class="nav-item <?php if($active == "recipe"){ echo "active";}?>">
-             <a class="nav-link" href="#">Receptek</a>
+             <a class="nav-link" href="recipes">Receptek</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
