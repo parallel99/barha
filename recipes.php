2f48df551824edd3a2ad909e765e651df7a2c9a7
baranyaimate <mateb06@gmail.com>
2019-10-11 21:36:31 +0200

    add recipes page

--- recipes.php
+++ recipes.php
@@ -19,6 +19,24 @@
                     <h6>Feltöltés: 2019. szeptember 20.</h6>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="/images/test-recipe.jpg">
+                </div>
+                <div class="col-sm-9">
+                    <h3>Almás pite</h3>
+                    <h6>Feltöltés: 2019. szeptember 20.</h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="/images/test-recipe.jpg">
+                </div>
+                <div class="col-sm-9">
+                    <h3>Almás pite</h3>
+                    <h6>Feltöltés: 2019. szeptember 20.</h6>
+                </div>
+            </div>
         </div>
     </body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
