476bae02df205799b1a705c14cc4bfa162f48844
baranyaimate <mateb06@gmail.com>
2019-10-11 21:41:04 +0200

    add recipes page

--- recipes.php
+++ recipes.php
@@ -10,32 +10,16 @@
             menu("recipes");
         ?>
         <div class="container recipe-list-container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <img src="/images/test-recipe.jpg">
-                </div>
-                <div class="col-sm-9">
-                    <h3>Almás pite</h3>
-                    <h6>Feltöltés: 2019. szeptember 20.</h6>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <img src="/images/test-recipe.jpg">
-                </div>
-                <div class="col-sm-9">
-                    <h3>Almás pite</h3>
-                    <h6>Feltöltés: 2019. szeptember 20.</h6>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3">
-                    <img src="/images/test-recipe.jpg">
-                </div>
-                <div class="col-sm-9">
-                    <h3>Almás pite</h3>
-                    <h6>Feltöltés: 2019. szeptember 20.</h6>
-                </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Feltöltés: 2019. szeptember 20.</h6>
+              </div>
             </div>
         </div>
     </body>
