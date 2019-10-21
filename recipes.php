e5bf2ea27df92a79b1d1df11d2347027d2a83347
baranyaimate <mateb06@gmail.com>
2019-10-21 21:01:08 +0200

    add lazy load to the images

--- recipes.php
+++ recipes.php
@@ -65,6 +65,259 @@
                   <h6>Elkészítési idő: <strong>30 perc</strong></h6>
               </div>
             </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
+            <div class="media">
+              <div class="media-left">
+                <a href="#">
+                  <img src="/images/test-recipe.jpg">
+                </a>
+              </div>
+              <div class="media-body">
+                  <h3>Almás pite</h3>
+                  <h6>Elkészítési idő: <strong>30 perc</strong></h6>
+              </div>
+            </div>
         </div>
     </body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
