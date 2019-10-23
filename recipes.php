39318d2497897a19942c4d68c8995a8e5ecfade5
baranyaimate <mateb06@gmail.com>
2019-10-23 14:23:36 +0200

    fix link

--- recipes.php
+++ recipes.php
@@ -10,6 +10,14 @@
             menu("recipes");
         ?>
         <div class="container recipe-list-container">
+            <div class="row search">
+                <div class="col-sm-10">
+                    <input class="form-control" type="text" placeholder="Keresés" name="search" aria-label="Keresés">
+                </div>
+                <div class="col-sm-2">
+                    <button type="submit" value="Keresés" name="submit" class="btn btn-primary">
+                </div>
+            </div>
             <a class="media" href="recipe?name=Almás%20pite">
               <div class="media-left">
                   <img src="/images/test-recipe.jpg" loading="lazy" alt="almás pite">
