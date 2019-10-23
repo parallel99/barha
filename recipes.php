8eec080ddf88f3ba4f749cb51d87a67da0056620
baranyaimate <mateb06@gmail.com>
2019-10-23 21:40:46 +0200

    add search

--- recipes.php
+++ recipes.php
@@ -10,12 +10,12 @@
             menu("recipes");
         ?>
         <div class="container recipe-list-container">
-            <div class="row search">
-                <div class="col-sm-10">
-                    <input class="form-control" type="text" placeholder="Keresés" name="search" aria-label="Keresés">
-                </div>
-                <div class="col-sm-2">
-                    <button type="submit" name="submit" class="btn btn-primary">Keresés</button>
+            <div class="row search no-gutters">
+                <div class="input-group">
+                    <input type="text" class="form-control">
+                       <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Go!</button>
+                       </span>
                 </div>
             </div>
             <a class="media" href="recipe?name=Almás%20pite">
