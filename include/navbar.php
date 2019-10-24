339bf83695e733f28a8b165afc6db9feb56bd335
baranyaimate <mateb06@gmail.com>
2019-10-24 16:35:13 +0200

    add alt and aria-label

--- include/navbar.php
+++ include/navbar.php
@@ -36,7 +36,7 @@ if(!isset($_SESSION['user'])){
 } else {
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img src="\images\navbar-logo.png" height="30"></a>
+    <a class="navbar-brand" href="/" aria-label="Főoldal"><img src="\images\navbar-logo.png" height="30" alt="BárHa"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
