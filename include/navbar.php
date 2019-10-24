e610bad3e911a5c5dd06eff34b5a2bf32e35049c
baranyaimate <mateb06@gmail.com>
2019-10-24 19:08:14 +0200

    change navbar logo

--- include/navbar.php
+++ include/navbar.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['user'])){
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/" aria-label="Főoldal">
         <img src="\images\android-chrome-512x512.png" width="30" height="30" class="d-inline-block align-top" alt="BárHa">
-        BárHa
+        <span class="name">BárHa</span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -41,7 +41,7 @@ if(!isset($_SESSION['user'])){
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="/" aria-label="Főoldal">
         <img src="\images\android-chrome-512x512.png" width="30" height="30" class="d-inline-block align-top" alt="BárHa">
-        BárHa
+        <span class="name">BárHa</span>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
