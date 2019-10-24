d6ce9b5483967e87a801feed987474fbda93375a
baranyaimate <mateb06@gmail.com>
2019-10-24 19:02:25 +0200

    change navbar logo

--- include/navbar.php
+++ include/navbar.php
@@ -39,7 +39,10 @@ if(!isset($_SESSION['user'])){
 } else {
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/" aria-label="Főoldal"><img src="\images\navbar-logo.png" height="30" alt="BárHa"></a>
+    <a class="navbar-brand" href="/" aria-label="Főoldal">
+        <img src="\images\android-chrome-512x512.png" width="30" height="30" class="d-inline-block align-top" alt="BárHa">
+        BárHa
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
