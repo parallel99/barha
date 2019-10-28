50444a8b8fe85f53f5489f535860468b5ec280db
Erik <assassinseries@gmail.com>
2019-10-28 12:43:39 +0100

    change database

--- recipes.php
+++ recipes.php
@@ -29,7 +29,7 @@
             if (isset($_GET['submit']) || isset($_GET['search'])) {
                 $search = trim($_GET['search']);
 
-                $sql = "SELECT * FROM ingredients WHERE LOWER(name) LIKE LOWER('%" . $search . "%') LIMIT 50;";
+                $sql = "SELECT * FROM recipebeta WHERE LOWER(name) LIKE LOWER('%" . $search . "%') LIMIT 50;";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute();
                 $data = $stmt->fetchAll();
@@ -57,7 +57,7 @@
                 }
 
             } else {
-                $sql = "SELECT * FROM ingredients;";
+                $sql = "SELECT * FROM recipebeta;";
                 $stmt = $pdo->prepare($sql);
                 $stmt->execute();
                 $data = $stmt->fetchAll();
