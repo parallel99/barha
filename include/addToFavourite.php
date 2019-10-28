1c344236f79da8732401a5238725f3222dbeaed2
baranyaimate <mateb06@gmail.com>
2019-10-28 12:19:18 +0100

    add favourite

--- include/addToFavourite.php
+++ include/addToFavourite.php
@@ -4,7 +4,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{:name}' WHERE email = :email;");
+$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || \'{:name}\' WHERE email = :email;");
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->bindValue(':email', $email, PDO::PARAM_STR);
 $stmt->execute();
