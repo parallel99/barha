b65cbb90b8d66088b053e05d54c113d57cf8cad3
Erik <assassinseries@gmail.com>
2019-10-29 14:34:11 +0100

    test select

--- upload_recipe.php
+++ upload_recipe.php
@@ -44,6 +44,14 @@ if(!isset($_SESSION['user'])){
                               }
                           ?>
                         </select>
+                        <select class="selectpicker" data-show-subtext="true" data-live-search="true">
+                            <option data-subtext="Rep California">Tom Foolery</option>
+                            <option data-subtext="Sen California">Bill Gordon</option>
+                            <option data-subtext="Sen Massacusetts">Elizabeth Warren</option>
+                            <option data-subtext="Rep Alabama">Mario Flores</option>
+                            <option data-subtext="Rep Alaska">Don Young</option>
+                            <option data-subtext="Rep California" disabled="disabled">Marvin Martinez</option>
+                          </select>
                     </div>
                 </div>
                 <script>
