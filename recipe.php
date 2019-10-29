8857e411f1dc87de110b362a1c2455eb4d6578cf
baranyaimate <mateb06@gmail.com>
2019-10-29 18:37:27 +0100

    add favourite

--- recipe.php
+++ recipe.php
@@ -46,14 +46,17 @@ if ($stmt->rowCount() != 1) {
 
                         print_r($data);
 
-                        echo "<h6 id=\"rows\">" . $stmt->rowCount() . "</h6>";
+                        $rows = $stmt->rowCount();
 
                         echo "<hr>";
                         if ($stmt->rowCount() == 0) {
                             echo "<div class=\"favourite\"><div style=\"background-image: url('/images/favourite.svg');\" class=\"favourite-star\"></div><h5 class=\"favourite-text\">Hozzáadás a kedvencekhez</h5></div>";
                         } else {
                             echo "<div class=\"favourite\"><div style=\"background-image: url('/images/favourite2.svg');\" class=\"favourite-star\"></div><h5 class=\"favourite-text\">Hozzáadva a kedvencekhez</h5></div>";
+                            $rows++;
                         }
+
+                        echo "<h6 id=\"rows\">" . $rows . "</h6>";
                     }
                     ?>
                     <hr>
