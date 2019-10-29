3819726976adb8e3d1337b5f540337f909b6719a
baranyaimate <mateb06@gmail.com>
2019-10-29 14:05:54 +0100

    add favourite

--- include/addToFavourite.php
+++ include/addToFavourite.php
@@ -1,15 +1,17 @@
 <?php
 
-include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
+/*include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-/*$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "';");
+$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "';");
 $stmt->execute();
 $data = $stmt->fetch();
 
 print_r($stmt->errorInfo());*/
 
-echo "UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "';";
+//echo "UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "';";
+
+echo "ez mazdmeg mokodok???";
 
 ?>
