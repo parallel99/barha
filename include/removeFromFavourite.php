6b316de0fb766795f133b6dbf74e390ad0abf423
baranyaimate <mateb06@gmail.com>
2019-10-29 20:34:52 +0100

    add favourite

--- include/removeFromFavourite.php
+++ include/removeFromFavourite.php
@@ -4,6 +4,6 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$stmt = $pdo->prepare("UPDATE users SET favourite = array_remove(favourite, '{" . $name . "}') WHERE email = '" . $email . "';");
+$stmt = $pdo->prepare("UPDATE users SET favourite = array_remove(favourite, '" . $name . "') WHERE email = '" . $email . "';");
 $stmt->execute();
 $data = $stmt->fetch();
