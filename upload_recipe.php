abd6f2afb2efe9d61f1f11af2b9ba7e24abda745
Erik <assassinseries@gmail.com>
2019-10-31 15:25:13 +0100

    change database charset

--- upload_recipe.php
+++ upload_recipe.php
@@ -86,8 +86,6 @@ $(".custom-file-input").on("change", function() {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
-// Az elkészítési idő ablak bezárása
-$('.clockpicker').clockpicker();
 </script>
 <?php
   function units()
