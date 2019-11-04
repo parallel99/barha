03dcf5221851d25af32f29b177043cc2ff043267
Erik <assassinseries@gmail.com>
2019-11-04 13:31:59 +0100

    image check

--- include/SaveRecipe.php
+++ include/SaveRecipe.php
@@ -84,27 +84,9 @@ class SaveRecipe {
         $this->msg .= '<div class="alert alert-danger alert-dismissible fade show">Nem Képet töltött fel!</div>';
     }
 
-    // Check file size
     if ($_FILES["customFile"]["size"] > 500000) {
         $this->msg .= '<div class="alert alert-danger alert-dismissible fade show">A kép nem lehet nagyobb 5MB-nál!</div>';
         $this->ok = false;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
     }
   }
 
