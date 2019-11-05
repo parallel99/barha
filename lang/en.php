e8a6ed39aeb77ed0ab68c24e16c433d01aaf9bb5
baranyaimate <mateb06@gmail.com>
2019-11-05 18:55:03 +0100

    add multilanguage support

---
+++ lang/en.php
@@ -0,0 +1,15 @@
+<?php
+
+define("_HOMEPAGE-TITLE", "Mi van a hűtőben?");
+define("_BARHA", "BarHa");
+define("_HOMEPAGE", "Homepage");
+define("_RECIPES", "Recipes");
+define("_FAVOURITES", "Favourites");
+define("_INGREDIENTS", "Ingredients");
+define("_PROFILE", "Profile");
+define("_LOGOUT", "Logout");
+define("_SEARCH", "Search");
+define("_REGISTER", "Register");
+define("_NAME", "Name");
+define("_USERNAME", "Username");
+define("_EMAIL", "E-mail");
---
+++ lang/hu.php
@@ -0,0 +1,15 @@
+<?php
+
+define("_HOMEPAGE-TITLE", "Mi van a hűtőben?");
+define("_BARHA", "BárHa");
+define("_HOMEPAGE", "Kezdőlap");
+define("_RECIPES", "Receptek");
+define("_FAVOURITES", "Kedvenceim");
+define("_INGREDIENTS", "Hozzávalók");
+define("_PROFILE", "Profil");
+define("_LOGOUT", "Kijelentkezés");
+define("_SEARCH", "Keresés");
+define("_REGISTER", "Regisztráció");
+define("_NAME", "Név");
+define("_USERNAME", "Felhasznalónév");
+define("_EMAIL", "E-mail");
