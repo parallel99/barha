c03047950b2372379449097924d536f4f79324db
baranyaimate <mateb06@gmail.com>
2019-11-08 20:32:22 +0100

    add 2 step auth

--- include/twoFactorAuthentication.php
+++ include/twoFactorAuthentication.php
@@ -31,6 +31,11 @@
       </div>
       <div class="modal-body">
           Ide kéne majd valami leírás
+          <?php
+              echo "<img class=\"auth-qr-code\" src=". $qrcode ." alt='Secret key'>";
+              echo "<p class=\"auth-secret small text-muted\">" . $secret . "</p>";
+          ?>
+          Meg ide is
           <div class="row">
             <div class="col-sm-6">
               <a href='https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Szerezd meg: Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/hu_badge_web_generic.png'/></a>
@@ -39,10 +44,6 @@
               <a href='https://apps.apple.com/us/app/google-authenticator/id388497605'><img alt='Szerezd meg: App Store' src='images/download-with-app-store.svg'/></a>
             </div>
           </div>
-          <?php
-              echo "<img class=\"auth-qr-code\" src=". $qrcode ." alt='Secret key'>";
-              echo "<p class=\"auth-secret small text-muted\">" . $secret . "</p>";
-          ?>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-dismiss="modal">Kész</button>
