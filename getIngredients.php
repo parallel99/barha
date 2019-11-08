b4b6197e50c2a337955c7a93c13c3a9b75400358
Erik <assassinseries@gmail.com>
2019-11-08 22:31:01 +0100

    fix ingredients array format

--- getIngredients.php
+++ getIngredients.php
@@ -3,9 +3,11 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 
 $stmt = $pdo->prepare("SELECT name FROM ingredients ORDER BY name");
 $stmt->execute();
-echo "<script>ingredients.push(";
+$ingredients = array();
+
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo '"'.$row['name'].'",';
+    array_push($ingredients, $row['name']);
 }
-echo ")</script>";
+
+echo '<script>ingredients.push("'.implode("\",\"",$arr).'")</script>';
 ?>
