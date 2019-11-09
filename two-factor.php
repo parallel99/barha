c0a2a5ef7e6389f2a6857c1ca6ba686bd79c70a5
baranyaimate <mateb06@gmail.com>
2019-11-09 11:32:52 +0100

    add two-factor.php

---
+++ two-factor.php
@@ -0,0 +1,36 @@
+<?php
+if (isset($_SESSION['user'])) {
+    header("Location: /");
+    die();
+}
+?>
+<!DOCTYPE html>
+<html lang="hu" role="main">
+    <head>
+        <title>BárHa | Bejelentkezés</title>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
+    </head>
+    <body>
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
+            menu("login");
+        ?>
+        <div class="form-container">
+            <form method="post" class="shadow" id="loginForm">
+                <div class="form-group">
+                    <label for="email">2 lépcsős hitelesítés</label>
+
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-1" id="2-step-auth-number-1" max="1" placeholder="" required>
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-2" id="2-step-auth-number-2" max="1" placeholder="" required>
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-3" id="2-step-auth-number-3" max="1" placeholder="" required>
+
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-4" id="2-step-auth-number-4" max="1" placeholder="" required>
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-5" id="2-step-auth-number-5" max="1" placeholder="" required>
+                    <input type="text" class="2-step-auth-number" name="2-step-auth-number-6" id="2-step-auth-number-6" max="1" placeholder="" required>
+                </div>
+                <button type="submit" class="btn btn-primary" name="submit">Bejelentkezés</button>
+            </form>
+        </div>
+    </body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
+</html>
