03aa1a1cb4e53f1f0ee1328c9f8e641b1e566ab2
Erik <assassinseries@gmail.com>
2019-11-09 22:14:37 +0100

    fix sql

--- include/addToFavourite.php
+++ include/addToFavourite.php
@@ -1,10 +1,12 @@
 <?php
 
 include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-$name = $_POST['name'];
+$name = '{'.$_POST['name'].'}';
 $email = $_SESSION['user']['email'];
 
-$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "' AND ('" . $name . "' != ALL(favourite) OR favourite IS NULL);");
+$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || :name WHERE email = :email AND ( :name != ALL(favourite) OR favourite IS NULL);");
+$stmt->bindParam(':name', $name, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
 $stmt->execute();//EZEN az sql-en még dolgozni kell
 $data = $stmt->fetch();
 
