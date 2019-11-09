879fca954f477aecb2fbca9bbafd8583ccbd374f
Erik <assassinseries@gmail.com>
2019-11-09 19:33:04 +0100

    add message

--- include/addToFavourite.php
+++ include/addToFavourite.php
@@ -4,8 +4,8 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $name = $_POST['name'];
 $email = $_SESSION['user']['email'];
 
-$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "' AND favourite NOT LIKE ANY('" . $name . "');");
-$stmt->execute();
+$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "';");
+$stmt->execute();//EZEN az sql-en még dolgozni kell
 $data = $stmt->fetch();
 
 ?>
