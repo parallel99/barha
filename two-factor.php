da57c7dac971d2aa97f163bc665a542481e17e70
baranyaimate <mateb06@gmail.com>
2019-11-09 12:38:35 +0100

    fix forms

--- two-factor.php
+++ two-factor.php
@@ -23,17 +23,17 @@ if (isset($_SESSION['user'])) {
                     <div class="row">
                         <div class="col-6">
                             <div class="row d-flex justify-content-end" id="left">
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-1" id="2-step-auth-number-1" maxlength="1" size="1" placeholder="" required autofocus>
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-2" id="2-step-auth-number-2" maxlength="1" size="1" placeholder="" required>
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-3" id="2-step-auth-number-3" maxlength="1" size="1" placeholder="" required>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-1" id="2-step-auth-number-1" maxlength="1" size="1" placeholder="" required autofocus>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-2" id="2-step-auth-number-2" maxlength="1" size="1" placeholder="" required>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-3" id="2-step-auth-number-3" maxlength="1" size="1" placeholder="" required>
                             </div>
                         </div>
 
                         <div class="col-6">
                             <div class="row d-flex justify-content-start" id="right">
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-4" id="2-step-auth-number-4" maxlength="1" size="1" placeholder="" required>
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-5" id="2-step-auth-number-5" maxlength="1" size="1" placeholder="" required>
-                                <input type="number" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-6" id="2-step-auth-number-6" maxlength="1" size="1" placeholder="" required>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-4" id="2-step-auth-number-4" maxlength="1" size="1" placeholder="" required>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-5" id="2-step-auth-number-5" maxlength="1" size="1" placeholder="" required>
+                                <input type="number" pattern="[0-9]*" class="two-step-auth-number form-control col-sm-2" name="2-step-auth-number-6" id="2-step-auth-number-6" maxlength="1" size="1" placeholder="" required>
                             </div>
                         </div>
                     </div>
