c174f1e3de307786ae5ebd077d215904fbd584e8
Erik <assassinseries@gmail.com>
2019-11-09 19:08:36 +0100

    fix sql issues

--- include/addToFavourite.php
+++ include/addToFavourite.php
@@ -4,7 +4,7 @@ include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $name = $_POST['name'];
 $email = $_SESSION['user']['email'];
 
-$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "' AND favourite NOT LIKE favourite || '{" . $name . "}';");
+$stmt = $pdo->prepare("UPDATE users SET favourite = favourite || '{" . $name . "}' WHERE email = '" . $email . "' AND favourite NOT LIKE ANY('" . $name . "');");
 $stmt->execute();
 $data = $stmt->fetch();
 
