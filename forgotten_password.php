1a2f8cd9a9446b7040aabf084777c13feafac879
baranyaimate <mateb06@gmail.com>
2019-11-10 21:01:14 +0100

    new password

--- forgotten_password.php
+++ forgotten_password.php
@@ -16,50 +16,50 @@ if (isset($_SESSION['user'])) {
             menu("none");
         ?>
         <div class="form-container forgotten-password-container">
+            <?php
+                if (isset($_POST['submit'])) {
+                    include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
+
+                    $new_password = generateRandomString(10);
+
+                    $sql = "UPDATE users SET password = :new_password WHERE email = :email;";
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+                    $stmt->bindValue(':new_password', hash("sha512", $new_password), PDO::PARAM_STR);
+                    $stmt->execute();
+                    $data = $stmt->fetch(PDO::FETCH_OBJ);
+
+                    echo $stmt->rowCount();
+
+                    if ($stmt->rowCount() == 0) {
+                        echo "<div class=\"alert alert-danger\" >Nincs ilyen email cím!</div>";
+                    }
+
+                    //send mail
+                    //ez csak ideiglenes
+                    echo $new_password;
+                }
+
+                function generateRandomString($length)
+                {
+                    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#%?.!*';
+                    $charactersLength = strlen($characters);
+                    $randomString = '';
+                    for ($i = 0; $i < $length; $i++) {
+                        $randomString .= $characters[rand(0, $charactersLength - 1)];
+                    }
+                    return $randomString;
+                }
+            ?>
             <form method="post" class="shadow" id="forgottenPasswordForm">
                 <h1>Elfelejtett jelszó</h1>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" class="form-control" name="email" id="email" placeholder="Email" autocomplete="email" required>
                 </div>
-                <input type="submit" class="btn btn-primary" value="Elfelejtettem a jelszavam">
+                <input type="submit" class="btn btn-primary" name="submit" value="Elfelejtettem a jelszavam">
             </form>
         </div>
-        <?php
-            if (isset($_POST['submit'])) {
-                include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-
-                $new_password = generateRandomString(10);
-
-                $sql = "UPDATE users SET password = :new_password WHERE email = :email;";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
-                $stmt->bindValue(':new_password', hash("sha512", $new_password), PDO::PARAM_STR);
-                $stmt->execute();
-                $data = $stmt->fetch(PDO::FETCH_OBJ);
-
-                echo $stmt->rowCount();
-
-                if ($stmt->rowCount() == 0) {
-                    echo "<div class=\"alert alert-danger\" >Nincs ilyen email cím!</div>";
-                }
-
-                //send mail
-                //ez csak ideiglenes
-                echo $new_password;
-            }
-
-            function generateRandomString($length)
-            {
-                $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@#%?.!*';
-                $charactersLength = strlen($characters);
-                $randomString = '';
-                for ($i = 0; $i < $length; $i++) {
-                    $randomString .= $characters[rand(0, $charactersLength - 1)];
-                }
-                return $randomString;
-            }
-        ?>
     </body>
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/include/footer.php'; ?>
 </html>
