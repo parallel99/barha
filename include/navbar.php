480fdc0c7392411c061f8b7ba8680af7d5391065
baranyaimate <mateb06@gmail.com>
2019-11-12 13:05:01 +0100

    navbar sticky top

--- include/navbar.php
+++ include/navbar.php
@@ -9,7 +9,7 @@ function menu($active) {
 
     if (!isset($_SESSION['user'])) {
         ?>
-        <nav class="navbar navbar-expand-md navbar-light bg-light" aria-label="navbar">
+        <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top" aria-label="navbar">
             <a class="navbar-brand" href="/" aria-label="Főoldal">
                 <img src="/images/android-chrome-192x192.png" width="30" height="30" class="d-inline-block align-top"
                      alt="BárHa">
@@ -47,7 +47,7 @@ function menu($active) {
         <?php
     } else {
         ?>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light" aria-label="navbar">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top" aria-label="navbar">
             <a class="navbar-brand" href="/" aria-label="Főoldal">
                 <img src="/images/android-chrome-192x192.png" width="30" height="30" class="d-inline-block align-top"
                      alt="BárHa">
