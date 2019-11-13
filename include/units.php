cf1b1e678f4c94f6ca46db80065f1b8df2779e13
baranyaimate <mateb06@gmail.com>
2019-11-13 22:41:38 +0100

    add TODO

--- include/units.php
+++ include/units.php
@@ -1,9 +1,11 @@
 <?php
 function units() {
 
+    //TODO ezt is le kene fordítani
+
     $unit = array("db", "g", "dkg", "kg", "liter", "dl", "cl", "ml",
         "merőkanál", "evőkanál", "kiskanál", "mokkáskanál",
-        "bögre", "csésze", "csipet", "marék", "gereszd", "csokor",
+        "bögre", "csésze", "csipet", "marék", "gerezd", "csokor",
         "csomag");
 
     return $unit;
