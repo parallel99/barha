1c0a42cac2c1c08b4ded046efb989fb11a1b1e89
Erik <assassinseries@gmail.com>
2019-11-13 20:50:27 +0100

    test custom select

--- MySecret.php
+++ MySecret.php
@@ -29,24 +29,16 @@ if (isset($_POST["submit"])) {
                 <div class="form-group">
                     <label class="newLine">Hozzávalók</label>
                     <input type="text" class="form-control ui-autocomplete-input upload-ingredients-name" name="ingredients1" id="ingredients1" placeholder="Hozzávaló" autocomplete="off">
-                    <input type="number" class="form-control ui-autocomplete-input upload-ingredients-db" name="db1" id="db1" placeholder="Mennyiség" min="1" max="5000" autocomplete="off">
-                    <select class="form-control ui-autocomplete-input upload-ingredients-unit" id="unit1" name="unit1" autocomplete="off" data-live-search="true">
-                        <?php
-                        foreach (units() as $unit) {
-                            echo "<option value='" . $unit . "'>" . $unit . "</option>";
-                        }
-                        ?>
-                    </select>
-                    <select class="selectpicker" data-live-search="true">
-                      <option data-tokens="ketchup mustard">Hot Dog, Fries and a Soda</option>
-                      <option data-tokens="mustard">Burger, Shake and a Smile</option>
-                      <option data-tokens="frosting">Sugar, Spice and all things nice</option>
+                    <input type="number" class="form-control ui-autocomplete-input upload-ingredients-db" name="db1" id="db1" placeholder="Mennyiség" min="1" max="5000">
+                    <select class="selectpicker" id="unit1" name="unit1" data-live-search="true">
+                      <?php
+                      foreach (units() as $unit) {
+                          echo "<option value='" . $unit . "'>" . $unit . "</option>";
+                      }
+                      ?>
                     </select>
 
                 </div>
-                <?php
-            }
-            ?>
         </div>
         <script>
             $('.ingredients-group').on('input', function () {
@@ -54,10 +46,10 @@ if (isset($_POST["submit"])) {
                 if ($("div.ingredients-group div:last-child > input").val() !== "" && length < 25) {
                     var inputs = "<div class='form-group'><input type='text' class='form-control upload-ingredients-name' name='ingredients" + (length + 1) + "' id='ingredients" + (length + 1) + "' placeholder='Hozzávaló'> ";
                     inputs += "<input type='number' pattern='\d*' class='form-control upload-ingredients-db' name='db" + (length + 1) + "' id='db" + (length + 1) + "' min='1' max='5000' placeholder='Mennyiség'> ";
-                    inputs += "<select class='form-control upload-ingredients-unit' id='unit" + (length + 1) + "' name='unit" + (length + 1) + "' autocomplete='off' data-live-search='true'>";
+                    inputs += "<select class='selectpicker' id='unit" + (length + 1) + "' name='unit" + (length + 1) + "' data-live-search='true'>";
                     <?php foreach (units() as $unit) {
-                    echo "inputs +=" . "\"<option value='" . $unit . "'>" . $unit . "</option>\";\r\n\t\t\t\t\t\t\t";
-                } ?>
+                      echo "inputs +=" . "\"<option value='" . $unit . "'>" . $unit . "</option>\";\r\n\t\t\t\t\t\t\t";
+                    } ?>
                     inputs += "</select></div>";
                     $(".ingredients-group").append(inputs);
                     $("#ingredients" + (length + 1)).autocomplete({
