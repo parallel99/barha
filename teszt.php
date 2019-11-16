07f2346b6283d96ebf981763b22c48e786425478
Erik <assassinseries@gmail.com>
2019-11-16 18:59:07 +0100

    test main search

--- teszt.php
+++ teszt.php
@@ -1,5 +1,5 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/include/navbar.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
 $sql = "CREATE TABLE orders (
    ID serial NOT NULL PRIMARY KEY,
    info json NOT NULL
