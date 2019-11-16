6d0fde87c83747fac9d6f9e63f1cf2cc424e2d00
baranyaimate <mateb06@gmail.com>
2019-11-16 11:48:26 +0100

    fix 2 factor auth

--- profile.php
+++ profile.php
@@ -159,6 +159,7 @@ menu("profile");
         <button type="button" name="account-delete" id="account-delete" class="btn btn-danger"><?= _DELETE_ACCOUNT ?></button>
     </form>
 </div>
+
 <!-- deleteConfirmModal -->
 <div class="modal fade" id="deleteConfirmModal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -179,6 +180,70 @@ menu("profile");
         </div>
     </div>
 </div>
+
+<!-- 2factorAuthModal -->
+<div id="auth-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Google Authenticator</h5>
+            </div>
+            <div class="modal-body">
+                Olvasd be a QR kódot a Google Authenticator alkalmazassal
+                <?php
+                echo "<img class=\"auth-qr-code\" src=" . $_SESSION['qrcode'] . " alt='Secret key'>";
+                echo "<p class=\"auth-secret small text-muted\">" . $_SESSION['secret'] . "</p>";
+                ?>
+                <h4 class="download-google-authenticator-text">Még nincs letöltve?</h4>
+                <h4 class="download-google-authenticator-text">Itt megteheted</h4>
+                <div class="row">
+                    <div class="col-6">
+                        <a href='https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img
+                                    alt='Szerezd meg: Google Play'
+                                    src='https://play.google.com/intl/en_us/badges/static/images/badges/hu_badge_web_generic.png'/></a>
+                    </div>
+                    <div class="col-6">
+                        <a href='https://apps.apple.com/us/app/google-authenticator/id388497605'><img
+                                    alt='Szerezd meg: App Store' src='/images/download-with-app-store.svg'/></a>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Mégsem</button>
+                <button type="button" class="btn btn-primary" name="saveSecretKey" id="saveSecretKey"
+                        data-dismiss="modal">Kész
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $('#auth-modal').modal('toggle');
+    $("body").css("padding-right", "0");
+
+    $("#saveSecretKey").click(function () {
+        $("#enable-2-step-auth").remove();
+        $.ajax({
+            url: 'include/saveSecretKey.php',
+            type: 'post',
+            data: {},
+            success: function () {
+            },
+            error: function () {
+            }
+        });
+        $('.account-2-step-auth-form').append("<input type=\"button\" class=\"btn btn-danger\" id=\"disable-2-step-auth\" value=\"Engedélyezve🎉\">");
+        $("#disable-2-step-auth")
+            .mouseover(function () {
+                $("#disable-2-step-auth").val("Kikapcsolás");
+            })
+            .mouseout(function () {
+                $("#disable-2-step-auth").val("Engedélyezve🎉");
+            });
+    });
+</script>
+
 <script>
     $("#account-delete").click(function () {
         $('.account-delete-error-container .alert').remove();
