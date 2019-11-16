c7a25c42a43dfd8f6791c2bed0bc08ab0a1fd5cb
Erik <assassinseries@gmail.com>
2019-11-16 19:19:58 +0100

    teszt json type

--- teszt.php
+++ teszt.php
@@ -1,5 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-$sql = "ALTER TABLE recipes ALTER COLUMN ingredients TYPE jsonb;";
+$sql = "ALTER TABLE recipes ADD COLUMN ingredientsteszt json;";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
