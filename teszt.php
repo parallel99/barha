078363b9593e3c287349affbf35ec39be4f3bc1f
Erik <assassinseries@gmail.com>
2019-11-16 19:05:56 +0100

    test main search

--- teszt.php
+++ teszt.php
@@ -1,8 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/include/db.php';
-$sql = "CREATE TABLE orders (
-   ID serial NOT NULL PRIMARY KEY,
-   info json NOT NULL
-);";
+$sql = "ALTER TABLE recipes ALTER COLUMN ingredients TYPE json;";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
